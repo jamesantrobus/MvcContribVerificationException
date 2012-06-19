a78f2714ec6b4a05e75e9fa1c4a59c543e7aa334
James Antrobus <jamesantrobus@gmail.com>
2012-06-19 12:14:28 +0100

    home controller with a couple of actions, added MvcContrib from NuGet

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿namespace Web.Controllers
+{
+	using MvcContrib;
+
+	using System.Web.Mvc;
+
+	public class HomeController : Controller
+    {       
+        public ActionResult Index()
+        {
+			return this.RedirectToAction(x => x.SomewhereElse());
+        }
+
+		public ActionResult SomewhereElse()
+		{
+			return View();
+		}
+    }
+}
